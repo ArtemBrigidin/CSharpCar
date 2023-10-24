4a938d87625d74f464056b02b85ff37188ec7a5d
ArtemBrigidin <artemgithuub@gmail.com>
2023-10-24 04:30:48 +0300

    Добавьте файлы проекта.

---
+++ CSharpCar/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+public interface IVehicle
+{
+    void Move();
+    void Refill(int oilCount);
+}
+
+public class Car : IVehicle
+{
+    private int oilCount;
+
+    public Car(int initialOilCount)
+    {
+        if (initialOilCount < 0)
+        {
+            throw new ArgumentException("Начальное количество бензина не может быть отрицательным.");
+        }
+        this.oilCount = initialOilCount;
+    }
+
+    public void Move()
+    {
+        if (oilCount >= 100)
+        {
+            Console.WriteLine("Автомобиль едет.");
+            oilCount -= 100; // Расходуем 100 единиц бензина при каждом вызове Move()
+        }
+        else
+        {
+            Console.WriteLine("Не достаточно бензина для передвижения.");
+        }
+    }
+
+    public void Refill(int oilCountToAdd)
+    {
+        if (oilCountToAdd < 0)
+        {
+            throw new ArgumentException("Объём добавляемого бензина не может быть отрицательным.");
+        }
+        oilCount += oilCountToAdd;
+        Console.WriteLine($"Залито {oilCountToAdd} единиц бензина. Текущее количество бензина: {oilCount}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Car myCar = new Car(200);
+
+        myCar.Move();
+        myCar.Refill(50);
+        myCar.Move();
+        myCar.Move();
+    }
+}
